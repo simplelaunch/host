da33385af978414d78dd8e71f78c9b4ad66f1cd1
Michael Mizner <michaelmizner@gmail.com>
2017-09-30 16:33:54 -0400

    Testing theme in plugin subdirectory

--- public/mu-plugins/add_themes-directory.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Plugin Name: Add Theme Directory
- *
- */
-register_theme_directory( '../themes' );
---
+++ public/plugins/add-themes-directory/add-themes-directory.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Plugin Name: Add Theme Directory
+ *
+ */
+register_theme_directory( dirname( __FILE__ ) . '/themes' );
