c1106f647dd2de3ccfb011e670a0a094bdf9285b
Michael Mizner <michaelmizner@gmail.com>
2017-09-30 17:25:19 -0400

    fresh copy of themes to sl folder

--- public/themes/parallax-pro/lib/customize.php
+++ public/themes/parallax-pro/lib/customize.php
@@ -1,72 +1,125 @@
 <?php
-
 /**
- * Customize Background Image Control Class
+ * Parallax Pro.
  *
- * @package WordPress
- * @subpackage Customize
- * @since 3.4.0
+ * This file adds the customizer additions to the Parallax Pro Theme.
+ *
+ * @package Parallax
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://my.studiopress.com/themes/parallax/
  */
-class Child_Parallax_Image_Control extends WP_Customize_Image_Control {
 
-	/**
-	 * Constructor.
-	 *
-	 * If $args['settings'] is not defined, use the $id as the setting ID.
-	 *
-	 * @since 3.4.0
-	 * @uses WP_Customize_Upload_Control::__construct()
-	 *
-	 * @param WP_Customize_Manager $manager
-	 * @param string $id
-	 * @param array $args
-	 */
-	public function __construct( $manager, $id, $args ) {
-		$this->statuses = array( '' => __( 'No Image', 'parallax' ) );
-
-		parent::__construct( $manager, $id, $args );
-
-		$this->add_tab( 'upload-new', __( 'Upload New', 'parallax' ), array( $this, 'tab_upload_new' ) );
-		$this->add_tab( 'uploaded',   __( 'Uploaded', 'parallax' ),   array( $this, 'tab_uploaded' ) );
-		
-		if ( $this->setting->default )
-			$this->add_tab( 'default',  __( 'Default', 'parallax' ),  array( $this, 'tab_default_background' ) );
-
-		// Early priority to occur before $this->manager->prepare_controls();
-		add_action( 'customize_controls_init', array( $this, 'prepare_control' ), 5 );
-	}
-
-	/**
-	 * @since 3.4.0
-	 * @uses WP_Customize_Image_Control::print_tab_image()
-	 */
-	public function tab_default_background() {
-		$this->print_tab_image( $this->setting->default );
-	}
-	
-}
+add_action( 'customize_register', 'parallax_customizer' );
+/**
+ * Add the theme settings and options to the Customizer.
+ *
+ * @since 1.0.0
+ */
+function parallax_customizer() {
 
 	global $wp_customize;
 
 	$images = apply_filters( 'parallax_images', array( '1', '3', '5' ) );
 	
-	$wp_customize->add_section( 'parallax-settings', array(
-		'title'    => __( 'Background Images', 'parallax' ),
-		'priority' => 35,
-	) );
+	$wp_customize->add_section(
+		'parallax-settings',
+		array(
+			'title'    => __( 'Front Page Background Images', 'parallax-pro' ),
+			'description' => __( '<p>Use the default images or personalize your site by uploading your own images for the front page background images.</p><p>The default images are <strong>1600 x 1050 pixels</strong>.</p>', 'parallax-pro' ),
+			'priority' => 35,
+		)
+	);
 
 	foreach( $images as $image ){
 
-		$wp_customize->add_setting( $image .'-image', array(
-			'default'  => sprintf( '%s/images/bg-%s.jpg', get_stylesheet_directory_uri(), $image ),
-			'type'     => 'option',
-		) );
+		$wp_customize->add_setting(
+			$image .'-image',
+			array(
+				'default'  => sprintf( '%s/images/bg-%s.jpg', get_stylesheet_directory_uri(), $image ),
+				'type'     => 'option',
+			)
+		);
 
-		$wp_customize->add_control( new Child_Parallax_Image_Control( $wp_customize, $image .'-image', array(
-			'label'    => sprintf( __( 'Featured Section %s Image:', 'parallax' ), $image ),
-			'section'  => 'parallax-settings',
-			'settings' => $image .'-image',
-			'priority' => $image+1,
-		) ) );
+		$wp_customize->add_control(
+			new WP_Customize_Image_Control(
+				$wp_customize,
+				$image .'-image',
+				array(
+					'label'    => sprintf( __( 'Featured Section %s Image:', 'parallax-pro' ), $image ),
+					'section'  => 'parallax-settings',
+					'settings' => $image .'-image',
+					'priority' => $image+1,
+				)
+			)
+		);
 
 	}
+
+	// Link color.
+	$wp_customize->add_setting(
+		'parallax_link_color',
+		array(
+			'default'           => parallax_get_default_accent_color(),
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'parallax_link_color',
+			array(
+				'description' => __( 'Change the color for content links, the hover color for linked titles, and more.', 'parallax-pro' ),
+				'label'       => __( 'Link Color', 'parallax-pro' ),
+				'section'     => 'colors',
+				'settings'    => 'parallax_link_color',
+			)
+		)
+	);
+
+	// Menu Link color.
+	$wp_customize->add_setting(
+		'parallax_menu_link_color',
+		array(
+			'default'           => parallax_get_default_accent_color(),
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'parallax_menu_link_color',
+			array(
+				'description' => __( 'Change the hover color for menu links and links in the footer area.', 'parallax-pro' ),
+				'label'       => __( 'Menu Link Color', 'parallax-pro' ),
+				'section'     => 'colors',
+				'settings'    => 'parallax_menu_link_color',
+			)
+		)
+	);
+
+	// Accent color.
+	$wp_customize->add_setting(
+		'parallax_accent_color',
+		array(
+			'default'           => parallax_get_default_accent_color(),
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'parallax_accent_color',
+			array(
+				'description' => __( 'Change the hover color for buttons, the footer widget background color, and more.', 'parallax-pro' ),
+				'label'       => __( 'Accent Color', 'parallax-pro' ),
+				'section'     => 'colors',
+				'settings'    => 'parallax_accent_color',
+			)
+		)
+	);
+
+}
