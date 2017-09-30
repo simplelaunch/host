29783a7e3f05418842f3e5c0edcff185e2d07f85
Michael Mizner <michaelmizner@gmail.com>
2017-09-30 17:08:27 -0400

    moved themes dir out of plugin

--- public/plugins/add-themes-directory/add-themes-directory.php
+++ public/plugins/add-themes-directory/add-themes-directory.php
@@ -3,4 +3,15 @@
  * Plugin Name: Add Theme Directory
  *
  */
-register_theme_directory( dirname( __FILE__ ) . '/themes' );
+
+register_theme_directory( '../themes' );
+
+add_filter( 'theme_root', 'sp8963532_theme_root' );
+function sp8963532_theme_root() {
+	return SL_THEME_PATH;
+}
+
+add_filter( 'theme_root_uri', 'sp8963532_theme_root_uri' );
+function sp8963532_theme_root_uri() {
+	return SL_THEME_URI;
+}
