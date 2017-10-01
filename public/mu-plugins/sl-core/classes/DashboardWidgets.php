17783b3c3cdb812a7cb0ba81c8a8db05623a9a61
Michael Mizner <michaelmizner@gmail.com>
2017-09-30 22:47:06 -0400

    Dashboard Widget Class

--- public/mu-plugins/sl-core/classes/DashboardWidgets.php
+++ public/mu-plugins/sl-core/classes/DashboardWidgets.php
@@ -6,6 +6,23 @@ class DashboardWidgets {
 
 	public function __construct() {
 		add_action( 'wp_dashboard_setup', [ $this, 'add_dashboard_widgets' ] );
+		add_action( 'admin_init', [ $this, 'removals' ] );
+
+		// remove_action( 'welcome_panel', 'wp_welcome_panel' ); // todo: what is this?
+		// remove_filter( 'update_footer', 'core_update_footer' ); // todo: what is this?
+	}
+
+	public function removals() {
+		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+		remove_meta_box( 'tribe_dashboard_widget', 'dashboard', 'normal' ); // Modern Tribe
 	}
 
 	public function add_dashboard_widgets() {
