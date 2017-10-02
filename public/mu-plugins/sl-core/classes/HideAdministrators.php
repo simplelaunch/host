11f6c01ec0b6986d2b6ba9624bbbb7a036a00129
Michael Mizner <michaelmizner@gmail.com>
2017-10-01 22:33:24 -0400

    Hide Administrator role from User list and add new

---
+++ public/mu-plugins/sl-core/classes/HideAdministrators.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mizner\SLC;
+
+class HideAdministrators {
+
+	public $current_user;
+
+	public function __construct( $current_user ) {
+
+		$this->current_user = $current_user;
+
+		add_action( 'pre_user_query', [ $this, 'users_page' ] );
+
+		add_filter( 'editable_roles', [ $this, 'add_new_user' ] );
+	}
+
+	public function add_new_user( $editable_roles ) {
+
+		global $pagenow;
+
+		if ( 'user-new.php' == $pagenow ) {
+			unset( $editable_roles['administrator'] );
+		}
+
+		return $editable_roles;
+
+	}
+
+	public function users_page( $user_search ) {
+
+		global $wpdb;
+
+		$user_search->query_where = str_replace(
+			'WHERE 1=1',
+			"WHERE 1=1 AND {$wpdb->users}.ID IN (
+              SELECT {$wpdb->usermeta}.user_id FROM $wpdb->usermeta 
+              WHERE {$wpdb->usermeta}.meta_key = '{$wpdb->prefix}capabilities'
+              AND {$wpdb->usermeta}.meta_value NOT LIKE '%administrator%' )",
+			$user_search->query_where
+		);
+	}
+
+}
