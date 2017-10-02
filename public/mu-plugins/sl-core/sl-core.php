afc4d80de6c5ff4856d1ec5f9d7c436808eac3d8
Michael Mizner <michaelmizner@gmail.com>
2017-10-02 08:43:57 -0400

    update core to run new classes

--- public/mu-plugins/sl-core/sl-core.php
+++ public/mu-plugins/sl-core/sl-core.php
@@ -20,6 +20,7 @@ define( __NAMESPACE__ . '\PATH', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\URI', str_replace( 'plugins/var/www/public/', '', plugin_dir_url( __FILE__ ) ) );
 
 require_once 'lib/autoloader.php';
+require_once 'lib/helpers.php';
 
 
 add_action( 'plugins_loaded', function () {
